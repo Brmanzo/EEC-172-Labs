c0478c984bd890efc0a69ec6621de84f2c95ef1f
TKE87775 <97363783+TKE87775@users.noreply.github.com>
2023-04-13 18:22:11 -0700

    Updated OLED
    Screen turns on, but only static

---
+++ Lab 2/Lab2/OLED/Debug/test.d
@@ -0,0 +1,69 @@
+# FIXED
+
+test.obj: ../test.c
+test.obj: C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+test.obj: C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+test.obj: C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+test.obj: C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+test.obj: C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_common_reg.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_ints.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/spi.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/interrupt.h
+test.obj: C:/TI/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h
+test.obj: ../Adafruit_GFX.h
+test.obj: ../Adafruit_SSD1351.h
+test.obj: ../glcdfont.h
+
+../test.c:
+
+C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/TI/ccs1220/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_types.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_memmap.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_common_reg.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/inc/hw_ints.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/spi.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_map.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/rom_patch.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/prcm.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/uart.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/driverlib/interrupt.h:
+
+C:/TI/CC3200SDK_1.5.0/cc3200-sdk/example/common/uart_if.h:
+
+../Adafruit_GFX.h:
+
+../Adafruit_SSD1351.h:
+
+../glcdfont.h:
+
