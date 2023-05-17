ddcc8af8303f65d23db27cec2004b80110dc5ab6
Brmanzo <76194522+Brmanzo@users.noreply.github.com>
2023-05-16 22:42:18 -0700

    Issues with sampling from microphone

--- Lab4/DTMF/DTMF/Debug/main.d
+++ Lab4/DTMF/DTMF/Debug/main.d
@@ -32,6 +32,7 @@ main.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/gpio.h
 main.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/systick.h
 main.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h
 main.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/timer.h
+main.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/example/common/timer_if.h
 main.obj: C:/ti/CC3200SDK_1.5.0/cc3200-sdk/example/common/gpio_if.h
 main.obj: ../Adafruit_GFX.h
 main.obj: ../Adafruit_SSD1351.h
@@ -70,6 +71,7 @@ C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/gpio.h:
 C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/systick.h: 
 C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/utils.h: 
 C:/ti/CC3200SDK_1.5.0/cc3200-sdk/driverlib/timer.h: 
+C:/ti/CC3200SDK_1.5.0/cc3200-sdk/example/common/timer_if.h: 
 C:/ti/CC3200SDK_1.5.0/cc3200-sdk/example/common/gpio_if.h: 
 ../Adafruit_GFX.h: 
 ../Adafruit_SSD1351.h: 
